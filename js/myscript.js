d6ff2f957050ddd5e1e9d9d19d84a591b6ac1954
LeppoXena <francesco.lepore@xenagames.com>
2022-05-03 15:49:31 +0200

    JS rules added., description added  - Completed!

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,17 @@
-let debug= "degugtxt";
-console.log(debug);
+// debug rules
+
+// let debug= "degugtxt";
+// console.log(debug);
+
+const pwname= prompt ("Ciao! Inserisci il tuo nome...");
+
+const pwsurname= prompt ("Grande! Inserisci il tuo cognome...");
+
+const pwcolor= prompt ("Ottimo" + pwname + ", ora scegli un colore ed è fatta!");
+
+const genpw=(pwname) + (pwsurname) + (pwcolor) + "21";  
+
+document.getElementById("pw").innerHTML=(genpw);
+
+console.log("la password segheta è: " + (genpw));
+
